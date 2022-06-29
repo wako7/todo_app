6ca0e5dff9eeb7c79a99920043889b35371daedf
wako7 <sxsyj924@gmail.com>
2022-06-29 20:26:45 +0000

    chage login layouts

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -11,4 +11,25 @@
   >
     <span class="navbar-toggler-icon"></span>
   </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+        @auth
+        <!-- ユーザ認証していればlogoutを表示 -->
+        <li class="nav-item">
+            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        </li>
+        @else
+        <!-- ユーザ認証をしていなければ登録とログインを表示 -->
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+        @endauth
+    </ul>
+</div>
 </header>
