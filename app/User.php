6e3e40955a52e198edccb2edbc855a4d698f9037
wako7 <sxsyj924@gmail.com>
2022-07-03 09:15:54 +0000

    impl relation

--- app/User.php
+++ app/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Todo');
     }
+    
+    public function tags()
+     {
+         return $this->hasMany('App\Tag');
+     }
 }
